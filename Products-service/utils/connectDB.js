190527d1b83a77d008f2dc4d694adcd1df9305c6
AbhirupRoy27 <abhiruproyofficial@gmail.com>
2025-12-06 22:16:35 +0530

    feat: add connect to db functionality.

---
+++ Products-service/utils/connectDB.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose'
+
+export async function connectDB() {
+  try {
+    const DB_URI = process.env.DB_URI
+    await mongoose.connect(DB_URI)
+    console.log(mongoose.connection.readyState)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export async function disConnectDB() {
+  try {
+    const DB_URI = process.env.DB_URI
+    await mongoose.disconnect(DB_URI)
+    console.log(mongoose.connection.readyState)
+  } catch (error) {
+    console.log(error)
+  }
+}
