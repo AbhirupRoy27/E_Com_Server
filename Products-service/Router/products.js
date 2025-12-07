2e3783d0a8e9a197814215e230de8e06678dad8f
AbhirupRoy27 <abhiruproyofficial@gmail.com>
2025-12-07 18:47:28 +0530

    fetch: defined the route to get all products from db and moved the default route to a seperate file

--- Products-service/Router/products.js
+++ Products-service/Router/products.js
@@ -1,30 +1,16 @@
 import { Router } from 'express'
-import { connectDB, disConnectDB } from '../utils/connectDB.js'
 import addProduct from '../controllers/addProduct.js'
+import getAllProduct from '../controllers/getAllProduct.js'
+import productDefault from '../controllers/productDefault.js'
 
 const ProductRouter = Router()
 
-ProductRouter.get('/', async (req, res) => {
-  try {
-    const id = req.query.id
-    if (!id) throw new Error('Request is incomplete or not complete')
-
-    await connectDB()
-    return res.status(200).json({
-      status: 'success',
-      message: 'Request Complete',
-      url: req.originalUrl,
-      id,
-    })
-  } catch (error) {
-    return res.status(404).json({
-      status: 'failure',
-      message: error.message,
-      url: req.originalUrl,
-    })
-  }
-})
-
 ProductRouter.post('/add-product', addProduct)
+
+ProductRouter.get('/all-product', getAllProduct)
+
+// ProductRouter.get('/product/:id', getProductById)
+
+ProductRouter.get('/', productDefault)
 
 export default ProductRouter
