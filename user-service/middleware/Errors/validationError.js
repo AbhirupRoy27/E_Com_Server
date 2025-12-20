3781d4c08170085f400ffbc6ac4aa2bbae234bde
AbhirupRoy27 <abhiruproyofficial@gmail.com>
2025-12-20 21:54:14 +0530

    feat(user): add centralized mongoose validation error middleware
    - Added validationError middleware to handle Mongoose ValidationError

---
+++ user-service/middleware/Errors/validationError.js
@@ -0,0 +1,17 @@
+export default function handleValidationError(error, req, res, next) {
+  if (error.name == 'ValidationError') {
+    const err = Object.keys(error.errors).length
+    if (err > 1) {
+      return res.status(400).json({
+        message: 'validation failed',
+        errors: err,
+      })
+    }
+    return res.status(400).json({
+      message: 'validation failed',
+      error: error.message,
+    })
+  }
+
+  next(error)
+}
