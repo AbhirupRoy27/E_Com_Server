8b041ff93c2d518751dde59169dfc40c6f0487a6
AbhirupRoy27 <abhiruproyofficial@gmail.com>
2025-12-24 22:20:33 +0530

    security(auth): prevent leaking user document on registration

--- user-service/controllers/userControllers/addUserController.js
+++ user-service/controllers/userControllers/addUserController.js
@@ -7,9 +7,22 @@ export default async function addUserController(req, res, next) {
     const hash = await hashPass(data.password)
     data.password = hash
     const insertedData = await User.create(data)
+
+    if (!insertedData) {
+      return res.status(500).json({
+        status: 'failure',
+        message: 'data is not added to DB',
+      })
+    }
+
     return res.status(200).json({
       status: 'success',
-      insertedData,
+      message: 'data is added to DB',
+      insertedData: {
+        username: insertedData.username,
+        email: insertedData.email,
+        isSubscribed: insertedData.isSubscribed,
+      },
     })
   } catch (error) {
     next(error)
