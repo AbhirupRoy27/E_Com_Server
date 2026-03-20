95301e508e5c214a71c94d7e0cc09979c15647ad
AbhirupRoy27 <abhiruproyofficial@gmail.com>
2026-03-21 03:55:48 +0530

    fix(controller): correct API message and update route details in userMetaInfoController

--- user-service/controllers/userControllers/userMetaInfoController.js
+++ user-service/controllers/userControllers/userMetaInfoController.js
@@ -1,30 +1,30 @@
 export default function userMetaInfoController(req, res) {
   return res.status(200).json({
     status: 'success',
-    message: 'API: Order-service Working.',
+    message: 'API: User-service Working.',
     possibleRoutes: [
-      {
-        name: '',
-        route: '/add-user',
-        devInfo: 'This Route handles adding User to DB',
-        method: 'POST',
-      },
-      {
-        name: '',
-        route: '/user-login',
-        devInfo: 'This Route handles the Order confirm related API calls.',
-        method: 'POST',
-      },
-      {
-        name: '',
-        route: '/get-user',
-        devInfo: 'This Route handles the Order confirm related API calls.',
-        method: 'GET',
-      },
       {
         name: 'Default Route',
         route: '/',
-        devInfo: 'This Route handle Default behaviour.',
+        devInfo: 'This Route handles default behaviour.',
+        method: 'GET',
+      },
+      {
+        name: 'Add User',
+        route: '/add-user',
+        devInfo: 'This Route handles adding a User to DB.',
+        method: 'POST',
+      },
+      {
+        name: 'User Login',
+        route: '/user-login',
+        devInfo: 'This Route handles User login and authentication.',
+        method: 'POST',
+      },
+      {
+        name: 'Get User',
+        route: '/get-user',
+        devInfo: 'This Route handles fetching a User from DB.',
         method: 'GET',
       },
     ],
